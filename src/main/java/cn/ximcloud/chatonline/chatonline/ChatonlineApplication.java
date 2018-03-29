15b99c979f098db9ef502ff9e9422037eeffb071
usami-mizugi <715759898@qq.com>
2018-03-29 14:18:12 +0800

    Initial commit

---
+++ src/main/java/cn/ximcloud/chatonline/chatonline/ChatonlineApplication.java
@@ -0,0 +1,12 @@
+package cn.ximcloud.chatonline.chatonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatonlineApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/ximcloud/chatonline/chatonline/ChatonlineApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.ximcloud.chatonline.chatonline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatonlineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
